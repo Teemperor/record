dbfa068dfe63aa1f71026e3413c18ce3954e04c9
Raphael Isemann <teemperor@gmail.com>
2017-12-16 23:52:28 +0100

    Force overwrite the retarted mpstat language setting

--- record.sh
+++ record.sh
@@ -5,8 +5,10 @@ cd "$(dirname ${BASH_SOURCE[0]})"
 
 export LANG=C
 export LC_TIME=C
+export S_TIME_FORMAT=ISO
 
-load=`mpstat -P ALL 5 1 | grep -v Average | grep all | awk '{print 100-$13}'`
+mp_line=`mpstat -P ALL 5 1 | grep -v Average | grep all`
+load=`echo "$mp_line" | awk '{print 100-$12}'`
 t=`date "+%F:%H:%M:%S"`
 mem=`free -m | head -n2 | tail -n1 | awk '{print $3}'`
 
